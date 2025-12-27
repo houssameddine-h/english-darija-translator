509ba38f97beefa2a73e9af232fd48a2bab6267a
Houssameddine <69154176+houssameddine-h@users.noreply.github.com>
2025-12-27 20:11:38 +0100

    typo fix

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGO_URL || 'mongodb://localhost:27017';
 
-const dbName = 'student-manager';
+const dbName = 'darija-translator';
 
 const client = new MongoClient(uri);
 
